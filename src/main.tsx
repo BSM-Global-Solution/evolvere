8603cb131c9dcff5dbe0974aab24b30e79ab5a2a
Moisés Barsoti <moises.barsot2@gmail.com>
2025-11-15 16:28:26 -0300

    chore(router): feito a instalação do react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './globals.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  { 
+    path: "/",
+    element: <App />,
+    errorElement: "erro",
+    children: [
+      {}
+  ]}
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </StrictMode>,
 )
