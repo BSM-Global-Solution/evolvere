b86fc8cf4ca47251987c76084c7b9e0bcc5e9979
Moisés Barsoti <moises.barsot2@gmail.com>
2025-11-16 11:07:24 -0300

    style(Header): feito a criação do menu hamburguer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,61 +1,59 @@
-import { MdOutlineDarkMode } from "react-icons/md";
+import { MdMenuOpen } from "react-icons/md";
 import { Link } from "react-router-dom";
-import LinkMenu from "../LinkMenu";
+import { TiThMenu } from "react-icons/ti";
+import { useState } from "react";
+import Menu from "../Menu";
+import EntrarCriarConta from "../EntrarCriarConta";
 
 export default function Header() {
+  
+  const [open, setOpen] = useState(false)
+  
   return (
-    <header className="fixed w-full px-17 pt-7 text-white font-inter z-1000">
+    <header className={`
+    fixed w-full px-17 pt-7 text-white font-inter z-1000
+    max-[1180px]:px-10 
+    max-[975px]:px-5 
+    ${
+      open ? 
+      "max-[862px]:h-full max-[862px]:px-0 max-[862px]:pt-0 max-[862px]:w-1/2 "
+      : ""
+    }  
+    `}>
       <nav className="
       bg-green-400 pl-6 pr-5 py-3 
       border-green-500 border-b-5 border-r-2 border-l-2
       rounded-b-2xl
+      max-[862px]:h-full
       ">
         <ul className="flex justify-between items-center font-bold">
           <li>
-            <Link to="/" className="text-3xl" title="Ir para o início">Evolvere</Link>
+            <Link to="/" title="Ir para o início"
+             className="
+             text-3xl max-[1091px]:text-2xl"
+             >
+              Evolvere
+            </Link>
           </li>
           <li>
-            <ul className="flex gap-10">
-              <li>
-                <LinkMenu to="/" title="Ir para o início">Home</LinkMenu>
-              </li>
-              <li>
-                <LinkMenu to="/" title="Ir para a página sobre">Sobre</LinkMenu>
-              </li>
-              <li>
-                <LinkMenu to="/" title="Ir para página de integrantes">Nossa Equipe</LinkMenu>
-              </li>
-              <li>
-                <LinkMenu to="/" title="Ir para a página de mentores">Mentores</LinkMenu>
-              </li>
-              <li>
-                <LinkMenu to="/" title="Ir para a página de contato">Contato</LinkMenu>
-              </li>
-            </ul>
+            <Menu className="max-[862px]:hidden" />
           </li>
           <li>
-            <ul className="flex gap-3 text-xl items-center">
-              <li>
-                <LinkMenu to="/" title="Clique aqui para entrar na conta">Entrar</LinkMenu>
-              </li>
-              <li>
-                <LinkMenu to="/" title="Clique aqui para criar sua conta"
-                  classname="bg-green-500 py-2 px-3 rounded-[5px]"
-                >
-                  Criar Conta
-                </LinkMenu>
-              </li>
-              <li>
-                <button 
-                className="cursor-pointer"
-                title="Clique para mudar para o modo Dark"
-                >
-                  <MdOutlineDarkMode size={35}/>
-                </button>
-              </li>
-            </ul>
+            <EntrarCriarConta className="max-[862px]:hidden items-center" />
+          </li>
+
+          <li className="hidden max-[862px]:block">
+            <button onClick={() => setOpen(!open)}>
+              {open ? <MdMenuOpen size={25} /> : <TiThMenu size={20} />}
+            </button>
           </li>
         </ul>
+
+        {/* Opções de menu do mobile */}
+        <Menu className={`${open ? "max-[862px]:flex-col mt-10" : "hidden min-[863px]:hidden"}`}/>
+
+        {/* Entrar e Criar conta do mobile */}
+        <EntrarCriarConta className={`${open ? "max-[862px]:flex-col mt-10" : "hidden min-[863px]:hidden"}`} />
       </nav>
     </header>
   );
