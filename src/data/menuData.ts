7e5ec7de855d2ae51861fd1728588846d9ae74b0
Moisés Barsoti <moises.barsot2@gmail.com>
2025-11-17 08:40:25 -0300

    feat(menuData): criado os dados do componente Menu

---
+++ src/data/menuData.ts
@@ -0,0 +1,15 @@
+interface MenuItem {
+  link: string;
+  title: string;
+  handleScrollLink?: string;
+  children: string;
+}
+
+
+export const menuData: MenuItem[] = [
+  {link: "/", title: "Ir para o início", handleScrollLink: "/", children: "Home"},
+  {link: "#sobre", title: "Ir para a página sobre", handleScrollLink: "sobre", children: "Sobre"},
+  {link: "#nossaEquipe", title: "Ir para página de integrantes", children: "Nossa Equipe"},
+  {link: "mentores", title: "Ir para página de mentores", children: "Mentores"},
+  {link: "contato", title: "Ir para a página de contato", children: "Contato"},
+];
