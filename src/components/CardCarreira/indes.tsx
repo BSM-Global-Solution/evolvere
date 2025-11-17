0257c0fc02be095ff8521e7c5e95712018e204a1
Moisés Barsoti <moises.barsot2@gmail.com>
2025-11-17 09:06:15 -0300

    style(CardCarreira): feito a responsividade dos cards

--- src/components/CardCarreira/indes.tsx
+++ src/components/CardCarreira/indes.tsx
@@ -10,7 +10,10 @@ export default function CardCarreira() {
     : planosDeCarreiraData.slice(0, 2); 
 
   return (
-    <div className="px-7.5 pt-50">
+    <div className="
+    px-7.5 pt-50
+    max-[445px]:px-2
+    ">
       <h6 className="text-white font-inter font-light text-3xl">
         Conheça alguns Planos de carreira para você se inspirar:
       </h6>
@@ -18,12 +21,16 @@ export default function CardCarreira() {
       <ul className="flex flex-col gap-10 mt-20">
         {categoriasVisiveis.map((planoCategoria, index) => (
           <li key={index}>
-            <h6 className="font-inter text-3xl text-white font-bold">
+            <h6 className="
+            font-inter text-3xl text-white font-bold
+            max-[503px]:text-2xl
+            max-[380px]:text-xl
+            ">
               {planoCategoria.categoria}
             </h6>
             <hr className="text-white border my-3" />
 
-            <ul className="flex justify-between flex-wrap gap-5">
+            <ul className="flex justify-between flex-wrap gap-5 max-[417px]:justify-center">
               {planoCategoria.planos.map((plano, indexPlano) => (
                 <li key={indexPlano}>
                   <Link to="" title="Clique aqui para ver esse plano">
@@ -32,6 +39,11 @@ export default function CardCarreira() {
                         w-108 h-80 bg-green-600 rounded-[20px]
                         border-2 border-white
                         hover:bg-green-600/80 duration-300
+                        max-[944px]:w-80 
+                        max-[717px]:w-70 
+                        max-[628px]:w-100 
+                        max-[417px]:w-90 
+                        max-[380px]:w-80 
                       "
                     >
                       <img
@@ -57,12 +69,18 @@ export default function CardCarreira() {
         ))}
       </ul>
 
-      {/* BOTÃO MOSTRAR MAIS / MENOS */}
-      <div className="flex justify-center items-center gap-4 py-14.5 text-white">
-        <p className="font-inter font-light text-2xl">
+      <div className="
+      flex justify-center items-center gap-4 py-14.5 text-white
+      max-[736px]:flex-col
+      max-[736px]:items-start
+      ">
+        <p className="
+        font-inter font-light text-2xl
+        max-[821px]:text-xl
+        ">
           {mostrarMais
             ? "Quer ver menos planos de carreira?"
-            : "Quer saber mais detalhes sobre cada Plano de Carreira?"}
+            : "Quer ver mais planos de carreira?"}
         </p>
 
         <button
