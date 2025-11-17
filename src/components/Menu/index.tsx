617503dad793d0001bd44b76d0094fe68ce4e1d6
Moisés Barsoti <moises.barsot2@gmail.com>
2025-11-17 08:39:50 -0300

    refactor(Menu): adicionado os dados do menu na pasta data

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -2,25 +2,11 @@
 import { useEffect, useRef } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import LinkMenu from "../LinkMenu";
-
-interface MenuItem {
-  link: string;
-  title: string;
-  handleScrollLink?: string;
-  children: string;
-}
+import { menuData } from "../../data/menuData";
 
 interface MenuProps {
   className?: string;
 }
-
-const menuData: MenuItem[] = [
-  {link: "/", title: "Ir para o início", handleScrollLink: "/", children: "Home"},
-  {link: "#sobre", title: "Ir para a página sobre", handleScrollLink: "sobre", children: "Sobre"},
-  {link: "#nossaEquipe", title: "Ir para página de integrantes", children: "Nossa Equipe"},
-  {link: "mentores", title: "Ir para página de mentores", children: "Mentores"},
-  {link: "contato", title: "Ir para a página de contato", children: "Contato"},
-];
 
 export default function Menu({ className }: MenuProps) {
   const navigate = useNavigate();
