a47ad6ccd21b4f985b173103b528fdb30f5e115f
Moisés Barsoti <moises.barsot2@gmail.com>
2025-11-17 19:53:43 -0300

    style(Footer): feito a responsividade do Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,13 +7,25 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <footer className="bg-green-600 w-full h-87 px-7.5 pt-5 pb-6">
-        <div className="flex">
-            <div className="w-1/2 flex flex-col justify-center items-center gap-3">
+    <footer className="
+    bg-green-600 w-full h-87 px-7.5 pt-5 pb-6
+    max-[900px]:h-75
+    max-[780px]:h-full
+    ">
+        <div className="
+        flex max-[1091px]:gap-8
+        max-[780px]:flex-col
+        ">
+            <div className="
+            w-1/2 flex flex-col justify-center items-center gap-3
+            max-[1091px]:w-1/3
+            max-[780px]:w-full
+            ">
                 <span>
                     <img
                      src="https://res.cloudinary.com/dtbgsboo5/image/upload/v1763390416/logo-evolvere_vjbdlq.png" 
                      alt="Logo do Evolvere"
+                     className="max-[780px]:w-full"
                     />
                 </span>
                 <ul className="flex justify-center items-center gap-5 w-full">
@@ -21,9 +33,15 @@ export default function Footer() {
                         <a 
                         href="" 
                         title="Ver o Twiter do Evolvere"
-                        className="flex bg-cyan-200 rounded-br-[15px] rounded-tl-[15px] pr-2 pt-2 pl-[7px] pb-[7px]"
+                        className="
+                        flex bg-cyan-200 rounded-br-[15px] rounded-tl-[15px] pr-2 pt-2 pl-[7px] pb-[7px]
+                        "
                         >
-                            <RiTwitterXFill className="text-[40px]"/>
+                            <RiTwitterXFill className="
+                            text-[40px]
+                            max-[1046px]:text-[30px] 
+                            max-[830px]:text-[20px] 
+                            "/>
                         </a>
                     </li>
                     <li>
@@ -32,7 +50,11 @@ export default function Footer() {
                         title="Ver o Twiter do Evolvere"
                         className="flex bg-cyan-200 rounded-br-[15px] rounded-tl-[15px] pr-2 pt-2 pl-[7px] pb-[7px]"
                         >
-                            <GrInstagram className="text-[40px]"/>
+                            <GrInstagram className="
+                            text-[40px]
+                            max-[1046px]:text-[30px]
+                            max-[830px]:text-[20px]
+                            "/>
                         </a>
                     </li>
                     <li>
@@ -41,13 +63,24 @@ export default function Footer() {
                         title="Ver o Twiter do Evolvere"
                         className="flex bg-cyan-200 rounded-br-[15px] rounded-tl-[15px] pr-2 pt-2 pl-[7px] pb-[7px]"
                         >
-                            <FaYoutube className="text-[40px]"/>
+                            <FaYoutube className="
+                            text-[40px]
+                            max-[1046px]:text-[30px]
+                            max-[830px]:text-[20px]
+                            " />
                         </a>
                     </li>
                 </ul>
             </div>
-            <div className="w-1/2 font-plus-jakarta-sans">
-                <ul className="flex justify-between">
+            <div className="
+            w-1/2 font-plus-jakarta-sans  
+            max-[1091px]:w-full
+            ">
+                <ul className="
+                flex justify-between 
+                max-[601px]:flex-wrap
+                max-[601px]:gap-5
+                ">
                     <li className="flex flex-col gap-2">
                         <h6 className="text-cyan-200 font-bold">Planos de Carreira para você</h6>
                         <ul className="flex flex-col gap-2 text-gray-100">
@@ -108,7 +141,7 @@ export default function Footer() {
                 </ul>
             </div>
         </div>
-        <div className="pt-3">
+        <div className="pt-3 max-[600px]:pt-7">
             <Link to="" className="flex justify-end items-center gap-0.5 text-white">
                 <MdOutlineMail />
                 <span>
