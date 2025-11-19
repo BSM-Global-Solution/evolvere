10fbff40e1bdc5abfcf9abf359b9e3b1adea245d
Moisés Barsoti <moises.barsot2@gmail.com>
2025-11-19 00:23:16 -0300

    feat(CriarConta)> criado a primeira section de apresentação

--- src/routes/CriarConta/index.tsx
+++ src/routes/CriarConta/index.tsx
@@ -1,7 +1,31 @@
 export default function CriarConta() {
     return (
-        <section>
-            criar conta
+        <section className="flex h-screen w-full">
+            <section className="bg-tertiary-700 h-full w-1/2">
+                <header className="
+                    flex flex-col justify-center items-center
+                    gap-12.5 h-full px-7
+                ">
+                    <h1 className="font-inter font-semibold text-white text-6xl text-center">
+                        Estamos felizes em ter você conosco!
+                    </h1>
+                    <figure className="flex flex-col items-center gap-12.5">
+                        <img 
+                        src="https://res.cloudinary.com/dtbgsboo5/image/upload/v1763522082/logo-fundo-transparente_nom0tb.png"
+                        alt="Logo da Evolvere com fundo transparente"
+                        className="w-[500px]"
+                         />
+                        <figcaption className="
+                            text-3xl text-white font-normal
+                        ">
+                            Comece agora e faça parte dessa evolução.
+                        </figcaption>
+                    </figure>
+                </header>
+            </section>
+            <section>
+
+            </section>
         </section>
     )
 }
