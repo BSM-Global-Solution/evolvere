8d232309c7790911214e1ad7281b4a26bbe66eae
FeKiModesto <fekimodesto04@gmail.com>
2025-11-21 20:51:30 -0300

    style(Contato): finalizando a estilização para a responsividade '2xl' na sessão de comentário e avaliação. Totalmente finalizado a responsividade da pagina de contato.

--- src/routes/Contato/index.tsx
+++ src/routes/Contato/index.tsx
@@ -202,16 +202,16 @@ export default function Contato() {
         </div>
       </div>
       <section className="flex flex-col items-center text-center p-4">
-        <h1 className="text-[var(--color-green-500)] font-bold text-xl lg:text-3xl xl:text-4xl">
+        <h1 className="text-[var(--color-green-500)] font-bold text-xl lg:text-3xl xl:text-4xl 2xl:text-5xl 2xl:py-1">
           O que você achou do nosso projeto?
         </h1>
-        <p className="text-[var(--color-green-500)] text-[16px] pb-5 lg:text-xl xl:text-2xl xl:px-60">
+        <p className="text-[var(--color-green-500)] text-[16px] pb-5 lg:text-xl xl:text-2xl xl:px-60 2xl:text-3xl 2xl:px-70 2xl:mt-2">
           Sua opinião é essencial para melhorarmos cada vez mais. <br />
           Conte pra gente como tem sido sua experiência com a Evolvere!
         </p>
-        <h2 className="text-[var(--color-green-500)] font-bold text-xl pb-3 lg:text-2xl xl:text-3xl">
+        <h2 className="text-[var(--color-green-500)] font-bold text-xl pb-3 lg:text-2xl xl:text-3xl 2xl:text-4xl">
           Gostaria de deixar um comentário? <br />
-          <span className="font-normal text-[16px] lg:text-xl xl:text-[22px]">
+          <span className="font-normal text-[16px] lg:text-xl xl:text-[22px] 2xl:text-[26px]">
             (Caso não queira, deixe este espaço em branco).
           </span>
         </h2>
@@ -219,7 +219,8 @@ export default function Contato() {
           name="Mensagem"
           id="Mensagem"
           placeholder="Digite sua mensagem"
-          className="w-full border-1 border-[var(--color-gray-200)] rounded-[10px] p-2 gap-1 text-[14px] lg:w-[50%] lg:py-3 lg:mt-1 xl:w-[45%] xl:py-4 xl:px-2 lg:mt-1.5"
+          className="w-full border-1 border-[var(--color-gray-200)] rounded-[10px] p-2 gap-1 text-[14px] lg:w-[50%] lg:py-3 lg:mt-1 xl:w-[45%] xl:py-4 xl:px-2 lg:mt-1.5 
+          2xl:text-xl 2xl:mt-1 2xl:py-2"
         ></textarea>
         <div className="py-3">
           <button
@@ -227,7 +228,7 @@ export default function Contato() {
             title="Clique aqui para enviar o feedback"
             className="w-full border-1 border-[var(--color-green-400)] bg-[var(--color-green-400)] rounded-[15px] text-[18px] text-white flex gap-3 p-1.5 
             hover:bg-[var(--color-green-300)] hover:duration-300
-            lg:p-3 lg:mt-1 xl:px-4 xl:py-3.5 xl:text-xl"
+            lg:p-3 lg:mt-1 xl:px-4 xl:py-3.5 xl:text-xl 2xl:text-2xl 2xl:p-4 2xl:items-center"
           >
             Enviar feedback <IoIosArrowDropright size={24} />
           </button>
