b99cd0a2cb13318cd034f4051f23e99c7c7db985
Sofia Siqueira <ssofiasiq@gmail.com>
2025-11-21 20:41:08 -0300

    feat(Integrantes): estilização da terceira section finalizada.

--- src/routes/Integrantes/index.tsx
+++ src/routes/Integrantes/index.tsx
@@ -229,28 +229,30 @@ export default function Integrantes() {
         <h2 className="text-3xl font-bold text-green-600 mb-3">
           O time do Evolvere:
         </h2>
-        <p className="text-green-600 mb-8">
+        <p className="text-green-600 mb-8 text-xl">
           Entenda como cada integrante contribuiu para a construção do Evolvere.
         </p>
 
-        <div>
+        <div className="flex flex-col gap-10 justify-center items-center">
           {cardsFinal.map((pessoa, index) => (
             <div
               key={index}
+              className="flex flex-col items-center gap-5 py-5 bg-green-300/35 cursor-pointer rounded-2xl border-4 border-green-500"
               >
               <img
                 src={pessoa.img}
                 alt={pessoa.nome}
+                className="w-[85%] border-4 rounded-[20px] border-green-500"
               />
 
-              <h3>
+              <h3 className="text-3xl text-green-600 font-bold">
                 {pessoa.nome}
               </h3>
 
               <ul>
                 {pessoa.funcoes.map((funcao, i) => (
                   <li key={i}
-                  className="list-disc">
+                  className="list-disc text-green-400 font-medium text-xl">
                     {funcao}
                   </li>
                 ))}
