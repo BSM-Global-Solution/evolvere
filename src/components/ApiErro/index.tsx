88165c994b731aba424054d8152c6ecd6db5feb6
Moisés Barsoti <moises.barsot2@gmail.com>
2025-11-21 11:11:37 -0300

    feat(ApiErro): criado o componente de erro da api

---
+++ src/components/ApiErro/index.tsx
@@ -0,0 +1,7 @@
+export default function ApiErro({ message }: { message: string }) {
+    return (
+        <div className="fixed top-7 right-3 p-3 rounded-lg bg-red-100 border border-red-500 text-red-700">
+            {message}
+        </div>
+    );
+}
