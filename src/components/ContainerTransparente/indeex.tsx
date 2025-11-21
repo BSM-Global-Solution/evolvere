e4613c34e3eba6c53c0a0883f9ce6337d6161b2b
Moisés Barsoti <moises.barsot2@gmail.com>
2025-11-21 18:57:24 -0300

    feat(ContainerTransparente): criado a prop className

--- src/components/ContainerTransparente/indeex.tsx
+++ src/components/ContainerTransparente/indeex.tsx
@@ -3,16 +3,18 @@ import type React from "react";
 interface ContainerTransparenteProps {
     width: number
     children: React.ReactNode
+    className?: string
 }
 
-export default function ContainerTransparente({children, width}: ContainerTransparenteProps) {
+export default function ContainerTransparente({children, width, className}: ContainerTransparenteProps) {
     return (
         <>
             <div className={`
-                w-${width} flex flex-col items-center pt-12.5 
+                w-${width} flex flex-col items-center pt-12.5 pb-12
                 bg-white/15 text-white border-2
                 border-white rounded-tl-[40px] rounded-br-[40px]
                 px-8
+                ${className ?? ""}
             `}>
                 {children}
             </div>
