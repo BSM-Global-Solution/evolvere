acb4f9c74e32cac3f304d7c20c9c3fdd6dd5fc4e
Sofia Siqueira <ssofiasiq@gmail.com>
2025-11-22 19:05:48 -0300

    feat(Error): iniciado estilização da página e alterado código de váriavel de cor.

--- src/routes/Error/index.tsx
+++ src/routes/Error/index.tsx
@@ -2,10 +2,12 @@ import { Link } from "react-router-dom";
 
 export default function Error() {
   return (
-    <section>
-      <div>
-        <h1>Página não encontrada!</h1>
-        <ul>
+    <section className="w-full h-screen bg-tertiary-700 px-45 py-24 text-white">
+      <div className="flex flex-col justify-between items-center w-full h-full px-42">
+        <h1 className="text-7xl font-bold text-center">
+            Página não encontrada!
+        </h1>
+        <ul className="flex flex-col gap-10 max-w-220 bg-tertiary-600 px-10 py-6 text-2xl rounded-br-[20px] rounded-tl-[20px] border-2 border-white">
           <li>
             Parece que a página que você está tentando acessar não existe ou não
             foi encontrada.
