9cf5e76103f2e2d58b4a18938ef99c1e7037bd51
Sofia Siqueira <ssofiasiq@gmail.com>
2025-11-22 18:44:47 -0300

    feat(Error): adicionado conteúdo da página de erro.

--- src/routes/Error/index.tsx
+++ src/routes/Error/index.tsx
@@ -1,7 +1,28 @@
+import { Link } from "react-router-dom";
+
 export default function Error() {
   return (
     <section>
+      <div>
         <h1>Página não encontrada!</h1>
+        <ul>
+          <li>
+            Parece que a página que você está tentando acessar não existe ou não
+            foi encontrada.
+          </li>
+          <li>
+            <ul>
+              <li>Verifique se o endereço digitado está correto.</li>
+              <li>Ou volte para a página inicial.</li>
+            </ul>
+          </li>
+        </ul>
+        <div>
+          <Link to="/" title="Voltar para a página inicial">
+            Ir para Home
+          </Link>
+        </div>
+      </div>
     </section>
   );
 }
