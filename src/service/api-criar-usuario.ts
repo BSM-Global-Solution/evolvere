b92412b20e923c0ed93d3931bf33f10484942acc
Moisés Barsoti <moises.barsot2@gmail.com>
2025-11-22 00:51:35 -0300

    feat(api-criar-usuario): alterado api-java para api-criar-usuario
    E colocado a url da api no .env

---
+++ src/service/api-criar-usuario.ts
@@ -0,0 +1,48 @@
+export interface CriarUsuarioDTO {
+  nome: string;
+  data: string;
+  email: string;
+  senha: string;
+}
+
+export interface Usuario {
+  id: number;
+  nome: string;
+  email: string;
+  dataDeNascimento: string;
+}
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL_JAVA as string;
+
+if (!BASE_URL) {
+  console.warn("VITE_API_BASE_URL_JAVA não está definida no .env!");
+}
+
+export async function criarUsuario(data: CriarUsuarioDTO) {
+  const response = await fetch(`${BASE_URL}/usuarios`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      nome: data.nome,
+      email: data.email,
+      senha: data.senha,
+      dataDeNascimento: data.data,
+    }),
+  });
+
+  if (!response.ok) {
+    let erroMensagem = "Erro ao criar usuário";
+
+    try {
+      const errorData = await response.json();
+      if (errorData.erro) erroMensagem = errorData.erro;
+      if (errorData.mensagem) erroMensagem = errorData.mensagem;
+    } catch (e) {}
+
+    throw new Error(erroMensagem);
+  }
+
+  return (await response.json()) as Usuario;
+}
