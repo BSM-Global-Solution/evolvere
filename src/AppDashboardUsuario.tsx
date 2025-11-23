92b89c1643b34bc4d722c4e3678955d4ebb04e1a
Moisés Barsoti <moises.barsot2@gmail.com>
2025-11-23 08:49:56 -0300

    feat(AppDashboardUsuario): criado useState para gerenciar a abertura do menu e o espaço do main

--- src/AppDashboardUsuario.tsx
+++ src/AppDashboardUsuario.tsx
@@ -1,11 +1,18 @@
 import { Outlet } from "react-router-dom";
 import HeaderUsuario from "./components/DashboardUsuario/HeaderUsuario";
+import { useState } from "react";
 
 export default function AppDashboardUsuario() {
+
+  const [open, setOpen] = useState<boolean>(false);
+
   return (
     <div className="min-h-screen flex">
-      <HeaderUsuario />
-      <main className="grow">
+      <HeaderUsuario open={open} setOpen={setOpen} />
+      <main className={`
+        grow bg-tertiary-100 duration-300
+        ${open ? "ml-[300px]" : "ml-[90px]"}
+      `}>
           <Outlet />
       </main>
     </div>
