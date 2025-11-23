9e9cd8e2d5402ca31baf45938684098ce3118891
Moisés Barsoti <moises.barsot2@gmail.com>
2025-11-23 09:31:04 -0300

    style(AppDashboardUsuario): aplicado o bg para o modo dark

--- src/AppDashboardUsuario.tsx
+++ src/AppDashboardUsuario.tsx
@@ -1,17 +1,24 @@
 import { Outlet } from "react-router-dom";
 import HeaderUsuario from "./components/DashboardUsuario/HeaderUsuario";
 import { useState } from "react";
+import { useTheme } from "./context/ThemeContextBase";
 
 export default function AppDashboardUsuario() {
 
+  const { theme } = useTheme();
   const [open, setOpen] = useState<boolean>(false);
 
   return (
     <div className="min-h-screen flex">
       <HeaderUsuario open={open} setOpen={setOpen} />
       <main className={`
-        grow bg-tertiary-100 duration-300
         ${open ? "ml-[300px]" : "ml-[90px]"}
+        ${
+          theme == "light"
+          ? "bg-tertiary-100"
+          : "bg-green-500"
+        }
+        grow  duration-300
       `}>
           <Outlet />
       </main>
