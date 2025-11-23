e78b51d6b12aeaae32730dc068f4c9ef839db226
Sofia Siqueira <ssofiasiq@gmail.com>
2025-11-23 11:00:57 -0300

    refactor(Integrantes): organizado código movendo array e componente para arquivos separados.

--- src/routes/Integrantes/index.tsx
+++ src/routes/Integrantes/index.tsx
@@ -1,51 +1,9 @@
 import { useState } from "react";
-import { FaPlus, FaMinus } from "react-icons/fa";
+import IntegranteInicioCard from "../../components/CardIntegrantesInicio";
+import { cardIntegrantes } from "../../data/integrantesInicioData";
 
 export default function Integrantes() {
-  interface CardsIntegrantes {
-    img: string;
-    figcaption: string;
-    icon: React.ReactNode;
-    role: string;
-    rm: string;
-    turma: string;
-    github: string;
-    linkedin: string;
-  }
-
-  const cardsPessoas: CardsIntegrantes[] = [
-    {
-      img: "https://avatars.githubusercontent.com/u/146322015?v=4",
-      figcaption: "Moisés Barsoti Andrade de Oliveira",
-      icon: <FaPlus />,
-      role: "Desenvolvedor Fullstack",
-      rm: "565049",
-      turma: "1TDSPG",
-      github: "https://github.com/moisesBarsoti",
-      linkedin: "https://www.linkedin.com/in/mois%C3%A9s-barsoti-468869278/",
-    },
-    {
-      img: "https://avatars.githubusercontent.com/u/202108239?v=4",
-      figcaption: "Sofia Siqueira Fontes",
-      icon: <FaPlus />,
-      role: "Designer UI/UX",
-      rm: "563829",
-      turma: "1TDSPG",
-      github: "https://github.com/sSofia-s",
-      linkedin: "https://www.linkedin.com/in/sofia-siqueira-0111a230b/",
-    },
-    {
-      img: "https://avatars.githubusercontent.com/u/202126798?v=4",
-      figcaption: "Felipe Kirschner Modesto",
-      icon: <FaPlus />,
-      role: "Desenvolvedor Back-end",
-      rm: "561810",
-      turma: "1TDSPG",
-      github: "https://github.com/FeKiModesto",
-      linkedin: "https://www.linkedin.com/in/felipe-modesto-348986366/",
-    },
-  ];
-
+  
   const [cardAtivo, setCardAtivo] = useState<number | null>(null);
 
   const toggleCard = (index: number) => {
@@ -97,95 +55,17 @@ export default function Integrantes() {
           Conheça os desenvolvedores por trás do projeto Evolvere:
         </p>
 
-        <div className="flex flex-col gap-10 justify-center items-center
-        md:flex-row">
-          {cardsPessoas.map((pessoa, index) => (
-            <div
-              key={index}
-              onClick={() => toggleCard(index)}
-              className="bg-green-600 cursor-pointer rounded-2xl border-4 border-green-500
-          transition-all duration-300 hover:scale-[1.02]"
-            >
-              <figure className="flex flex-col items-center gap-5 pt-5">
-                <img
-                  src={pessoa.img}
-                  alt={pessoa.figcaption}
-                  className={`w-[85%] border-4 rounded-[20px] border-green-500
-                transition-all duration-300
-                ${
-                  cardAtivo === index
-                    ? "opacity-0 h-0 overflow-hidden"
-                    : "opacity-100"
-                }
-              `}
-                />
-
-                <figcaption className="text-cyan-100 text-2xl text-center w-[90%]">
-                  <strong>
-                    {cardAtivo === index
-                      ? pessoa.figcaption
-                      : cortarNome(pessoa.figcaption)}
-                  </strong>
-                </figcaption>
-              </figure>
-
-              {cardAtivo === index && (
-                <div className="text-cyan-100 px-5 flex flex-col animate-fade-in duration-600 mt-4">
-                  <h2 className="text-xl text-green-200 font-medium self-center">
-                    {pessoa.role}
-                  </h2>
-                  <hr className="w-[95%] self-center" />
-
-                  <ul className="text-xl mt-3 mb-5">
-                    <li>
-                      <strong>RM:</strong> {pessoa.rm}
-                    </li>
-                    <li>
-                      <strong>Turma:</strong> {pessoa.turma}
-                    </li>
-                  </ul>
-
-                  <p className="text-2xl">
-                    <strong>Redes Sociais:</strong>
-                  </p>
-                  <div className="gap-3 flex flex-row py-4">
-                    <a
-                      href={pessoa.github}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <img
-                        src="https://res.cloudinary.com/dt26mfzpw/image/upload/v1763756821/icon-github_y8ytrf.png"
-                        alt="Logotipo do GitHub"
-                      />
-                    </a>
-
-                    <a
-                      href={pessoa.linkedin}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <img
-                        src="https://res.cloudinary.com/dt26mfzpw/image/upload/v1763756820/icon-linkedin_ueql1w.png"
-                        alt="Logotipo do LinkedIn"
-                      />
-                    </a>
-                  </div>
-
-                  <button
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      setCardAtivo(null);
-                    }}
-                  ></button>
-                </div>
-              )}
-              <span className="text-cyan-100 flex justify-end pr-5 pb-3 text-xl">
-                {cardAtivo === index ? <FaMinus /> : <FaPlus />}
-              </span>
-            </div>
-          ))}
-        </div>
+        <div className="flex flex-col gap-10 justify-center items-center md:flex-row">
+        {cardIntegrantes.map((pessoa, index) => (
+          <IntegranteInicioCard
+            key={index}
+            pessoa={pessoa}
+            ativo={cardAtivo === index}
+            onToggle={() => toggleCard(index)}
+            cortarNome={cortarNome}
+          />
+        ))}
+      </div>
 
         <h3 className="text-3xl font-bold text-green-500 mt-10 mb-5
         sm:text-4xl">
