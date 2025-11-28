2cd83b22bd022b820995fbda12f4ff5353ab0d8a
Moisés Barsoti <moises.barsot2@gmail.com>
2025-11-27 23:32:37 -0300

    feat(HeaderUsuarioData): adicionado o link para anotações

--- src/data/headerUsuarioData.tsx
+++ src/data/headerUsuarioData.tsx
@@ -1,4 +1,5 @@
 import { BsPeople } from "react-icons/bs";
+import { CiEdit } from "react-icons/ci";
 import { GoHome } from "react-icons/go";
 import { IoMdLogOut } from "react-icons/io";
 import { IoSettingsOutline } from "react-icons/io5";
@@ -8,6 +9,7 @@ import { RiRobot2Line, RiWechatLine } from "react-icons/ri";
 export const HeaderUsuarioData = [
     {icon: <GoHome size={30} />, label: "Home", link: "/dashboardUsuario", title: "Ir para o Home"},
     {icon: <RiRobot2Line size={30} />, label: "Mentor.IA", link: "/mentorIA", title: "Ir para o Mentor.IA"},
+    {icon: <CiEdit size={30} />, label: "Anotações", link: "/anotacoes", title: "Ir para Anotações"},
     {icon: <LuPlaneTakeoff size={30} />, label: "Planos de Carreira", link: "/planosDeCarreira", title: "Ir para os Planos de Carreira"},
     {icon: <RiWechatLine size={30} />, label: "Chats", link: "/chats", title: "Ir para os Chats"},
     {icon: <BsPeople size={30} />, label: "Mentores", link: "/mentores", title: "Ir para os Mentores"},
