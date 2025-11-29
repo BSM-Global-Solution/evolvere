dfaa7a60378be0dc3824799bf6585fe51f68204b
Moisés Barsoti <moises.barsot2@gmail.com>
2025-11-29 10:39:30 -0300

    feat(Configuracoes): adicionado as informações do card perfil
    E colocado a função onClose no icon de fechar

--- src/components/DashboardUsuario/Configuracoes/index.tsx
+++ src/components/DashboardUsuario/Configuracoes/index.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { CgCloseO, CgProfile } from "react-icons/cg";
 import { HiOutlinePaintBrush } from "react-icons/hi2";
 import { MdOutlineManageAccounts } from "react-icons/md";
+import { useUser } from "../../../context/UserContext";
 
 interface ConfiguracoesProps {
     open: boolean
@@ -10,6 +11,7 @@ interface ConfiguracoesProps {
 
 export default function Configuracoes({open, onClose}: ConfiguracoesProps) {
 
+    const { usuario } = useUser();
     const [active, setActive] = useState<"perfil" | "conta" | "aparencia">("perfil");
     if (!open) return null;
 
@@ -18,7 +20,7 @@ export default function Configuracoes({open, onClose}: ConfiguracoesProps) {
             inset-0 w-full h-full bg-black/40
             flex justify-center items-center
         ">
-        <div className="bg-white flex gap-10 pl-4 pr-7 pt-9.5 w-250 rounded-[20px]">
+        <div className="bg-white flex gap-10 pl-4 pr-7 pt-9.5 pb-31 w-250 rounded-[20px]">
             <nav>
                 <ul className="font-inter flex flex-col gap-7.5">
                     <li>
@@ -79,6 +81,7 @@ export default function Configuracoes({open, onClose}: ConfiguracoesProps) {
                      <div className="flex justify-end">
                         <button 
                         title="Fechar"
+                        onClick={onClose}
                         className="text-green-500 cursor-pointer hover:text-green-500/40 duration-300">
                             <CgCloseO size={30} />
                         </button>
@@ -86,8 +89,32 @@ export default function Configuracoes({open, onClose}: ConfiguracoesProps) {
                      <h1 className="text-4xl text-green-500 font-bold">Perfil</h1>
                      <hr className="border-2 border-green-500" />
                 </div>
-                <div>
-                    
+                <div className="flex pt-8">
+                    <div className="flex flex-col gap-10 w-1/2 pt-2.5">
+                        <div>
+                            <h2 className="text-green-500 font-bold text-2xl mb-2.5">Id do usuário:</h2>
+                            <div className="
+                            py-2 pl-3.5 w-[45%] bg-tertiary-400 rounded-[10px]
+                            border-2 border-green-500
+                            ">
+                                <span className="font-inter text-xl">
+                                    {usuario?.id}
+                                </span>
+                            </div>
+                        </div>
+                        <div>
+                            <h2 className="text-green-500 font-bold text-2xl mb-2.5">Nome</h2>
+                            <span className="font-inter text-xl">
+                                Usuário: {usuario?.nome}
+                            </span>
+                        </div>
+                        <div>
+                            <h2 className="text-green-500 font-bold text-2xl mb-2.5">Data de Nacimento:</h2>
+                            <span className="font-inter text-xl">
+                                {usuario?.dataDeNascimento}
+                            </span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
