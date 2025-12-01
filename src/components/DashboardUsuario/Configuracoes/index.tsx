7442c0c969fff3bdaf3db7c77075c9509abbccd2
Moisés Barsoti <moises.barsot2@gmail.com>
2025-12-01 11:52:08 -0300

    refactor(Configuracoes): adicionado o componente ConfigPerfil
    E adicionado lógica para que somente apareça o configPerfil quando o perfil estiver ativo

--- src/components/DashboardUsuario/Configuracoes/index.tsx
+++ src/components/DashboardUsuario/Configuracoes/index.tsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
-import { CgCloseO, CgProfile } from "react-icons/cg";
+import { CgProfile } from "react-icons/cg";
 import { HiOutlinePaintBrush } from "react-icons/hi2";
 import { MdOutlineManageAccounts } from "react-icons/md";
-import { useUser } from "../../../context/UserContext";
-import TituloConfiguracoes from "../TituloConfiguracoes";
+import ConfigPerfil from "../ConfigPerfil";
 
 interface ConfiguracoesProps {
     open: boolean
@@ -12,7 +11,6 @@ interface ConfiguracoesProps {
 
 export default function Configuracoes({open, onClose}: ConfiguracoesProps) {
 
-    const { usuario } = useUser();
     const [active, setActive] = useState<"perfil" | "conta" | "aparencia">("perfil");
     if (!open) return null;
 
@@ -77,43 +75,9 @@ export default function Configuracoes({open, onClose}: ConfiguracoesProps) {
                     </li>
                 </ul>
             </nav>
-            <div className="font-inter w-full">
-                <TituloConfiguracoes title="Perfil" onClose={onClose} />
-                <div className="flex pt-8">
-                    <div className="flex flex-col gap-10 w-1/2 pt-2.5">
-                        <div>
-                            <h2 className="text-green-500 font-bold text-2xl mb-2.5">Id do usuário:</h2>
-                            <div className="
-                            py-2 pl-3.5 w-[45%] bg-tertiary-400 rounded-[10px]
-                            border-2 border-green-500
-                            ">
-                                <span className="font-inter text-xl">
-                                    {usuario?.id}
-                                </span>
-                            </div>
-                        </div>
-                        <div>
-                            <h2 className="text-green-500 font-bold text-2xl mb-2.5">Nome</h2>
-                            <span className="font-inter text-xl">
-                                Usuário: {usuario?.nome}
-                            </span>
-                        </div>
-                        <div>
-                            <h2 className="text-green-500 font-bold text-2xl mb-2.5">Data de Nacimento:</h2>
-                            <span className="font-inter text-xl">
-                                {usuario?.dataDeNascimento}
-                            </span>
-                        </div>
-                    </div>
-                    <div>
-                        <img 
-                            src="https://res.cloudinary.com/dtbgsboo5/image/upload/v1764299508/icone-simples-do-usuario-3d-isolado_169241-6922_mm1dis.avif" 
-                            alt=""
-                            className="w-[280px] rounded-full border-2 border-green-500"
-                        />
-                    </div>
-                </div>
-            </div>
+        {
+            active === "perfil" ? <ConfigPerfil onClose={onClose} /> : ""
+        }
         </div>
         </dialog>
     )
