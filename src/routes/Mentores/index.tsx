3eb9a46d300b53ef407398beb06b9b20ed29b11d
Moisés Barsoti <moises.barsot2@gmail.com>
2025-12-03 11:20:05 -0300

    style(Mentores): feito a responsividade da página mentores

--- src/routes/Mentores/index.tsx
+++ src/routes/Mentores/index.tsx
@@ -3,12 +3,21 @@ import { mentores } from "../../data/mentoresData";
 
 export default function Mentores() {
   return (
-    <section>
+    <section className="max-[535px]:px-3">
       <header className="flex flex-col items-center gap-3 pt-[140px] font-inter">
-        <h1 className="text-5xl font-bold text-green-500">
+        <h1 className="
+        text-5xl font-bold text-green-500
+        max-[765px]:text-4xl
+        max-[535px]:text-center
+        ">
           Conheça nossos Mentores
         </h1>
-        <p className="text-3xl text-green-500">
+        <p className="
+        text-3xl text-green-500
+        max-[765px]:text-2xl
+        max-[614px]:text-xl
+        max-[535px]:text-center
+        ">
           Especialistas prontos para orientar você em sua jornada.
         </p>
       </header>
@@ -17,6 +26,7 @@ export default function Mentores() {
       pt-[90px] px-12.5 pb-[70px]
       flex justify-between flex-wrap
       gap-20
+      max-[900px]:justify-center
       ">
         {mentores.map((mentor) => (
           <li key={mentor.id}>
@@ -29,17 +39,19 @@ export default function Mentores() {
                 flex flex-col items-center gap-5
                 px-[26px] rounded-[25px] pb-5 pt-[30px]
                 text-white hover:bg-green-400/80 duration-300
+                max-[1008px]:w-[380px]
+                max-[940px]:w-[360px]
               ">
                 <img 
                   src={mentor.foto} 
                   alt="Uma fotografia do mentor"
-                  className="w-[220px] rounded-[10px]"
+                  className="w-[220px] rounded-[10px] max-[940px]:w-[210px]"
                 />
                 <div className="flex flex-col items-center gap-2.5">
-                  <figcaption className="font-bold text-2xl">
+                  <figcaption className="font-bold text-2xl max-[940px]:text-xl">
                     {mentor.nome} - {mentor.cargo}
                   </figcaption>
-                  <p className="">
+                  <p className="max-[940px]:text-lg">
                     {mentor.descricao}
                   </p>
                 </div>
