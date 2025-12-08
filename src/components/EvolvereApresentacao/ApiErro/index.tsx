fcddb23af52112feef38c3241f66f671373bdca6
Moisés Barsoti <moises.barsot2@gmail.com>
2025-12-08 00:52:09 -0300

    style(ApiErro): adicionado o texto para branco

--- src/components/EvolvereApresentacao/ApiErro/index.tsx
+++ src/components/EvolvereApresentacao/ApiErro/index.tsx
@@ -1,6 +1,6 @@
 export default function ApiErro({ message }: { message: string }) {
     return (
-        <div className="fixed top-7 right-3 p-3 rounded-lg bg-red-100 border border-red-500 text-red-700">
+        <div className="fixed top-7 right-3 p-3 rounded-lg bg-red-100 border border-red-500 text-white">
             {message}
         </div>
     );
